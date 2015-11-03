879b976f449e77bb09ab7c5eb6e0ac6544be5bd2
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-03 18:59:18 +0200

    Autoboxing

---
+++ Day_4_Autoboxing/Test_1_4.java
@@ -0,0 +1,11 @@
+
+public class Test_1_4 {
+
+	public static void main(String[] args) {
+		
+		
+	Integer dig1 = 6;
+	Integer dig2 = new Integer(5);
+	
+	}
+}
---
+++ Day_4_Autoboxing/Test_1_7.java
@@ -0,0 +1,13 @@
+package Day_4_Frame_Autoboxing;
+
+public class Test_1_7 {
+	
+	public static void main(String[] args) {
+		
+		Integer dig1 = 5;
+		Integer dig2 = new Integer(5);
+		
+		
+	}
+
+}
