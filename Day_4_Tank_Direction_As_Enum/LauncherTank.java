d5cf59116cb2cf75ac50b2f7f55598ec03a89b8b
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-03 23:45:35 +0200

    Tank_Direction_As_Enum

---
+++ Day_4_Tank_Direction_As_Enum/LauncherTank.java
@@ -0,0 +1,11 @@
+package Day_4_Tank_Direction_As_Enum;
+
+public class LauncherTank {
+	
+	public static void main(String[] args) throws Exception{
+		ActionField af = new ActionField();
+		af.runTheGame();
+
+}
+
+}
