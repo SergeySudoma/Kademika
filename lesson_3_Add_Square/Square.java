f5c05a455922953d7f60e43978c079c88ed2b93f
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-13 00:26:21 +0200

    lesson_3_add_square

--- lesson_3_Add_Square/Square.java
+++ lesson_3_Add_Square/Square.java
@@ -3,12 +3,11 @@ package lesson_3_Add_Square;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Square extends Shape{
+public class Square extends Rectangle{
 
 //	@Override
 //	public void draw(Graphics g) {
 //		g.setColor(Color.BLACK);
-//		g.drawRect(23, 270, 50, 50);
-	
-	}
+//		g.drawRect(23, 270, 50, 50);	
+//	}
 }
