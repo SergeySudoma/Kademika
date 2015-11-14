061a2438202fc7d9f6abdc29dd7632a2d83ba1d2
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-14 18:24:07 +0200

    lesson_3_Defender (t34)

---
+++ lesson_3_Defender/T34.java
@@ -0,0 +1,17 @@
+package lesson_3_Defender;
+
+public class T34 extends AbstractTank{
+
+	public T34(BattleField bf, ActionField af, int x, int y, Direction direction) {
+		super(bf, af, x, y, direction);
+		
+	}
+	
+	public T34(BattleField bf, ActionField af) {
+		super(bf, af);
+		
+	}
+	
+	
+
+}
