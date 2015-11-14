e925a08266d5288b62f7a6481f6004a512a81bf9
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-14 20:45:16 +0200

    day_4_Abstract_Tank

---
+++ day_4_Abstract_Tank/BT7.java
@@ -0,0 +1,20 @@
+package day_4_Abstract_Tank;
+
+public class BT7 extends AbstractTank{
+
+	private int speed = 2 * getSpeed();
+	
+	public BT7(BattleField bf, ActionField af){
+		super(bf, af);
+	}
+	
+	public BT7(BattleField bf, ActionField af, int x, int y, Direction direction) {
+		super(bf, af, x, y, direction);
+		
+	}	
+	
+	public int getSpeed(){
+		return speed;
+	}
+	
+}
---
+++ day_4_Abstract_Tank/Tiger.java
@@ -0,0 +1,26 @@
+package day_4_Abstract_Tank;
+
+public class Tiger extends AbstractTank{
+	
+	private int armor = 2;
+
+	public Tiger(BattleField bf, ActionField af, int x, int y,
+			Direction direction) {
+		super(bf, af, x, y, direction);
+		
+	}
+	
+	public int getArmor(){
+		return armor;
+	}
+	
+	public int delArmor(){
+		return armor = armor - 1;
+	}
+	
+	
+	public void fly(){
+		System.out.println("flyyy");
+	}
+	
+}
