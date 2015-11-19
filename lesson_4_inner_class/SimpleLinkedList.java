7b9105f28e3f5babbb43cb23d69778a26ef490de
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-19 19:28:19 +0200

    lesson_4_inner_class

---
+++ lesson_4_inner_class/SimpleLinkedList.java
@@ -0,0 +1,39 @@
+package lesson_4_inner_class;
+
+public class SimpleLinkedList {
+	
+	Node root;
+	int size;
+	
+	public SimpleLinkedList(){
+		size = 0;
+	}
+	
+	
+	public int getSize(){
+		return size;
+	}
+	
+	public void addFirst(Object obj){
+		
+	}
+	
+	public void addLast(Object obj){
+		
+	}
+	
+	public void addAfter(Object obj, Object previous){
+		
+	}
+	
+	public class Node {
+		
+		private Object obj;
+		Node node;
+		
+		public Node(){
+			
+		}
+
+	}
+}
