0d1332a1fd83686c589e7b32c5c8f078a05a72ed
sergey.sudoma <sergey.sudoma@gmail.com>
2015-11-19 23:21:43 +0200

    lesson_4_addFirst_to_linkedList
    There is an inaccuracy in task concerning checking emptyness of existing
    list. My opinion is that it's unnecessary to check it adding first
    element.

---
+++ lesson_4_addFirst/SimpleLinkedList.java
@@ -0,0 +1,43 @@
+package lesson_4_addFirst;
+
+public class SimpleLinkedList {
+	
+	private Node root;
+	private int size;
+	
+	public SimpleLinkedList(){
+		size = 0;
+	}
+	
+	
+	public int getSize(){
+		return size;
+	}
+	
+
+	public void addFirst(Object obj){		
+		Node nodeFirst = new Node();		
+		nodeFirst.obj = obj;
+		nodeFirst.ref = root;
+		root = nodeFirst.ref;
+	}
+	
+	public void addLast(Object obj){
+		
+	}
+	
+	public void addAfter(Object obj, Object previous){
+		
+	}
+	
+	private class Node {
+		
+		private Object obj;
+		private Node ref;
+		
+		public Node(){			
+			
+		}
+
+	}
+}
