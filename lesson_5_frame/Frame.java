3fb704d9f1356983018fa587d99239a608d61499
sergey.sudoma <sergey.sudoma@gmail.com>
2015-12-08 22:44:53 +0200

    draw_frame

---
+++ lesson_5_frame/Frame.java
@@ -0,0 +1,16 @@
+package lesson_5_frame;
+
+import javax.swing.JFrame;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(800, 600);
+		frame.setLocation(300, 100);
+
+	}
+
+}
