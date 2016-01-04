4b904ce3500634007c88c89ae57b3dd8c345e997
sergey.sudoma <sergey.sudoma@gmail.com>
2016-01-04 17:23:55 +0200

    lesson_6_add_menu

---
+++ lesson_6_add_menu/Model.java
@@ -0,0 +1,6 @@
+package lesson_6_add_menu;
+
+public enum Model {
+	AUDI_A6, BMW_X5, OPEL_KADETT, SUBARU_FORESTER, DODGE_NEON
+	
+}
---
+++ lesson_6_add_menu/Warehouse.java
@@ -0,0 +1,58 @@
+package lesson_6_add_menu;
+
+import java.util.ArrayList;
+
+public class Warehouse {
+	
+	private ArrayList<Car> carList;
+	
+	public Warehouse(){
+		carList = new ArrayList<Car>();
+		fillWarehouse();
+	}
+	
+	private void fillWarehouse(){
+		carList.add(new Car(Model.AUDI_A6));
+		carList.add(new Car(Model.BMW_X5));
+		carList.add(new Car(Model.DODGE_NEON));
+		carList.add(new Car(Model.OPEL_KADETT));
+		carList.add(new Car(Model.SUBARU_FORESTER));
+		carList.add(new Car(Model.SUBARU_FORESTER));
+	}
+	
+	public void addCar(Car car){
+		carList.add(car);
+	}
+	
+	public void removeCar(Car car, int quantity){
+		if(checkAvailability(car, quantity)){
+			for(int i = 1; i <= quantity; i++){
+				for(Car c : carList){
+					if(car.getModel() == (c.getModel())){
+						carList.remove(c);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public boolean checkAvailability(Car car, int quantity){
+		int count = 0;
+		for(Car c : carList){
+			if(car.getModel() == c.getModel()){
+				count++;
+				if(count >= quantity){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	
+	public ArrayList<Car> getcarList(){
+		return carList;
+	}
+	
+}
