3c5b7a023609aa558e043289b781507bb6c811aa
SergeySudoma <sergey.sudoma@gmail.com>
2016-03-14 09:44:25 +0200

    arrayTest

---
+++ Tests/ArrayTest.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		String s = " hello ";
+		
+		xInc(x, s);
+		
+		System.out.println(x + " " + s + " main");
+		
+	}
+		
+		public static void xInc(int x, String s){
+			x =+ 1;
+			s = " megaass ";
+		}
+}
